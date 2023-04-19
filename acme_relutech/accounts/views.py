8c40fa6007198f7c9a70514fc184e3bb4af06b3e
juliobarbagallo <julio.barbagallo@gmail.com>
2023-04-19 18:01:16 -0300

    feat: Ads model, template and urls for accounts

--- acme_relutech/accounts/views.py
+++ acme_relutech/accounts/views.py
@@ -1,3 +1,38 @@
-from django.shortcuts import render
+from rest_framework import generics
+from django.contrib.auth.views import LoginView
+from .models import CustomUser
+from accounts.forms import CustomUserCreationForm
+from django.shortcuts import redirect, render
+from django.contrib.auth import authenticate, login
+from django.contrib.auth.decorators import login_required
 
-# Create your views here.
+
+def register(request):
+    if request.method == "POST":
+        form = CustomUserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get("username")
+            password = form.cleaned_data.get("password1")
+            user = authenticate(username=username, password=password)
+            login(request, user)
+            return redirect("dashboard")
+    else:
+        form = CustomUserCreationForm()
+    return render(request, "registration/register.html", {"form": form})
+
+
+class CustomLoginView(LoginView):
+    template_name = "registration/login.html"
+
+
+@login_required
+def dashboard(request):
+    user = request.user
+    
+    
+    context = {
+        "user": user,
+        
+    }
+    return render(request, "dashboard.html", context)
