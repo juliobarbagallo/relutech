7b1b1fddd913fad2f5572fd98e1fe40a51063788
juliobarbagallo <julio.barbagallo@gmail.com>
2023-04-20 09:49:37 -0300

    feat: Adds assets mdoel and tests

--- acme_relutech/assets/models.py
+++ acme_relutech/assets/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from accounts.models import CustomUser
 
-# Create your models here.
+class Asset(models.Model):
+    LAPTOP = 'laptop'
+    KEYBOARD = 'keyboard'
+    MOUSE = 'mouse'
+    HEADSET = 'headset'
+    MONITOR = 'monitor'
+    ASSET_TYPE_CHOICES = [
+        (LAPTOP, 'Laptop'),
+        (KEYBOARD, 'Keyboard'),
+        (MOUSE, 'Mouse'),
+        (HEADSET, 'Headset'),
+        (MONITOR, 'Monitor'),
+    ]
+
+    id = models.AutoField(primary_key=True)
+    brand = models.CharField(max_length=50)
+    model = models.CharField(max_length=50)
+    type = models.CharField(max_length=50, choices=ASSET_TYPE_CHOICES)
+    developer = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
+
