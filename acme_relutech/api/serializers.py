f0985f4ab16ec1883797fa85e0250fedf5e8bff1
juliobarbagallo <julio.barbagallo@gmail.com>
2023-04-20 11:39:08 -0300

    feat: Adds asset set to developers

--- acme_relutech/api/serializers.py
+++ acme_relutech/api/serializers.py
@@ -1,7 +1,20 @@
 from rest_framework  import serializers
 from accounts.models import CustomUser
+from assets.models import Asset
 
 class DeveloperSerializer(serializers.ModelSerializer):
     class Meta:
         model = CustomUser
         fields = ['last_login', 'email', 'username', 'is_active']
+
+class AssetSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Asset
+        fields = ['id', 'brand', 'model', 'type', 'developer']
+
+class DeveloperWithAssetsSerializer(serializers.ModelSerializer):
+    assets = AssetSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = CustomUser
+        fields = ['id', 'username', 'email', 'assets']
--- acme_relutech/api/views.py
+++ acme_relutech/api/views.py
@@ -7,7 +7,9 @@ from rest_framework.generics import ListCreateAPIView
 from rest_framework.permissions import IsAuthenticated
 from accounts.models import CustomUser
 from accounts.forms import CustomUserCreationForm
-from .serializers import DeveloperSerializer
+from .serializers import DeveloperSerializer, AssetSerializer, DeveloperWithAssetsSerializer
+
+from assets.models import Asset
 
 
 # TODO: Rename classes to friendly ones. install flake8, black and isort
@@ -28,13 +30,14 @@ class DeveloperListCreateAPIView(APIView):
     permission_classes = [IsAuthenticated]
 
     def get(self, request):
-        print(request.user.is_authenticated)
         if not request.user.is_authenticated:
             return Response(status=status.HTTP_401_UNAUTHORIZED)
         if not request.user.is_admin:
             return Response(status=status.HTTP_403_FORBIDDEN)
-        developers = CustomUser.objects.filter(is_admin=False)
-        serializer = DeveloperSerializer(developers, many=True)
+        # developers = CustomUser.objects.filter(is_admin=False)
+        # serializer = DeveloperSerializer(developers, many=True)
+        developers = CustomUser.objects.filter(is_admin=False).prefetch_related('assets')
+        serializer = DeveloperWithAssetsSerializer(developers, many=True)
         return Response(serializer.data)
 
     def post(self, request):
@@ -68,3 +71,53 @@ class DeveloperListCreateAPIView(APIView):
             return Response(serializer.data)
         else:
             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        
+
+class AssetAssignmentsAPIView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request, developer_id):
+        if not request.user.is_authenticated or not request.user.is_admin:
+            return Response({'error': 'Not authorized'}, status=status.HTTP_401_UNAUTHORIZED)
+
+        developer = CustomUser.objects.filter(pk=developer_id, is_admin=False).first()
+        if developer is None:
+            return Response({'error': 'Developer not found'}, status=status.HTTP_404_NOT_FOUND)
+
+        assets = Asset.objects.filter(developer=developer)
+        serializer = AssetSerializer(assets, many=True)
+        return Response(serializer.data)
+
+    def post(self, request, developer_id):
+        if not request.user.is_authenticated or not request.user.is_admin:
+            return Response({'error': 'Not authorized'}, status=status.HTTP_401_UNAUTHORIZED)
+
+        developer = CustomUser.objects.filter(pk=developer_id, is_admin=False).first()
+        if developer is None:
+            return Response({'error': 'Developer not found'}, status=status.HTTP_404_NOT_FOUND)
+
+        serializer = AssetSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save(developer=developer)
+            assets = Asset.objects.filter(developer=developer)
+            serializer = AssetSerializer(assets, many=True)
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, developer_id, asset_id):
+        if not request.user.is_authenticated or not request.user.is_admin:
+            return Response({'error': 'Not authorized'}, status=status.HTTP_401_UNAUTHORIZED)
+
+        developer = CustomUser.objects.filter(pk=developer_id, is_admin=False).first()
+        if developer is None:
+            return Response({'error': 'Developer not found'}, status=status.HTTP_404_NOT_FOUND)
+
+        asset = Asset.objects.filter(pk=asset_id, developer=developer).first()
+        if asset is None:
+            return Response({'error': 'Asset not found'}, status=status.HTTP_404_NOT_FOUND)
+
+        asset.delete()
+        assets = Asset.objects.filter(developer=developer)
+        serializer = AssetSerializer(assets, many=True)
+        return Response(serializer.data)
